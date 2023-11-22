51b5e16dff4019afd1cc0d0328ac71b3c1f07bca
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2023-11-22 17:29:36 +0100

    Fix some issues

--- src/main/java/com/tecknobit/pandoro/helpers/ui/UIUtils.kt
+++ src/main/java/com/tecknobit/pandoro/helpers/ui/UIUtils.kt
@@ -30,10 +30,29 @@ const val YELLOW_COLOR: String = "#bfae19"
  */
 const val RED_COLOR: String = "#A81515"
 
+/**
+ * The **ListManager** interface is useful to manage a list of items in the UI
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ */
 interface ListManager {
 
+    /**
+     * Function to refresh a list of items to display in the UI
+     *
+     * No-any params required
+     */
     fun refreshValues()
 
+    /**
+     * Function to check whether the list to display in the UI need to be refreshed due changes
+     *
+     * @param currentList: the current list displayed
+     * @param newList: the new hypothetical list to set and display
+     * @param T: the type of the items of the lists
+     *
+     * @return whether refresh the list as [Boolean]
+     */
     fun <T : PandoroItemStructure> needToRefresh(currentList: List<T>, newList: List<T>): Boolean {
         return ((currentList.isEmpty() && newList.isNotEmpty()) ||
                 (JSONArray(currentList).toString() != JSONArray(newList).toString()))
@@ -41,10 +60,29 @@ interface ListManager {
 
 }
 
+/**
+ * The **SingleItemManager** interface is useful to manage a single item in the UI
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ */
 interface SingleItemManager {
 
+    /**
+     * Function to refresh an item to display in the UI
+     *
+     * No-any params required
+     */
     fun refreshItem() {}
 
+    /**
+     * Function to check whether the item to display in the UI need to be refreshed due changes
+     *
+     * @param currentItem: the current item displayed
+     * @param newItem: the new hypothetical item to set and display
+     * @param T: the type of the item
+     *
+     * @return whether refresh the item as [Boolean]
+     */
     fun <T : PandoroItemStructure> needToRefresh(currentItem: T, newItem: T): Boolean {
         return currentItem.toString() != newItem.toString()
     }
